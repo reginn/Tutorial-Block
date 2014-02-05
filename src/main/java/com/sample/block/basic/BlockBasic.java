0b2d75e285cf2dc55a5bb7d7cfbdb5a461ed466d
reginn666 <reginn666@gmail.com>
2014-02-05 22:04:41 +0900

    {}の位置を変更

--- src/main/java/com/sample/block/basic/BlockBasic.java
+++ src/main/java/com/sample/block/basic/BlockBasic.java
@@ -7,8 +7,10 @@ import net.minecraft.block.material.Material;
  * Blockのコンストラクタがprotectedになっているので,
  * 初期化用の単純なクラスを作る.
  */
-public class BlockBasic extends Block {
-	public BlockBasic(Material material) {
+public class BlockBasic extends Block
+{
+	public BlockBasic(Material material)
+    {
 		super(material);
 	}
 }
